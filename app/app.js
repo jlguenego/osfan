14479d27ed1eb5afdbfb8ef87d817a48e503fe84
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-03-21 15:58:13 +0100

    ui validate

--- app/app.js
+++ app/app.js
@@ -1,7 +1,18 @@
 (function() {
 	'use strict';
 
-	var app = angular.module('mainApp', []);
+	var app = angular.module('mainApp', ['ui.validate']);
+	
+	app.run(['$rootScope', function($rootScope) {
+		$rootScope.isCountry = function(value) {
+			console.log('$rootScope.country', $rootScope.country);
+			var countries = ['France', 'Italie'];
+			if (countries.indexOf(value) >= 0) {
+				return true;
+			}
+			return false;
+		};
+	}]);
 
 	app.directive('myHeader', function() {
 		return {
