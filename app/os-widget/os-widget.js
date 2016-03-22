f026d4d2cea1132584378ec3099aac38183759ac
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-03-22 12:32:00 +0100

    5 STARS

--- app/os-widget/os-widget.js
+++ app/os-widget/os-widget.js
@@ -6,8 +6,17 @@
 	app.directive('osStar', function() {
 		return {
 			restrict: 'CEA',
-			link: function() {
-				console.log('os-star link');
+			link: function(scope, element) {
+				console.log('os-star link', arguments);
+				var note = 3;
+				var html = '';
+				for (var i = 0; i < note; i++) {
+					html += '<img src="os-widget/img/yellow_star.png" />';
+				}
+				for (var i = note; i < 5; i++) {
+					html += '<img src="os-widget/img/white_star.png" />';
+				}
+				element.html(html);
 			}
 		};
 	});
