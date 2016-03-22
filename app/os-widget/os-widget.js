639cea84a3587483ce92f8c70e67433f7718e2a4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-03-22 14:16:42 +0100

    watch

--- app/os-widget/os-widget.js
+++ app/os-widget/os-widget.js
@@ -8,17 +8,22 @@
 			restrict: 'CEA',
 			link: function(scope, element, attrs) {
 				console.log('os-star link', arguments);
-				var note = Number(attrs.note);
-				note = (note > 5) ? 5 : note;
-				note = (note < 0) ? 0 : note;
-				var html = '';
-				for (var i = 0; i < note; i++) {
-					html += '<img src="os-widget/img/yellow_star.png" />';
-				}
-				for (var i = note; i < 5; i++) {
-					html += '<img src="os-widget/img/white_star.png" />';
-				}
-				element.html(html);
+				
+				scope.$watch('myNote', function() {
+					var note = Number(scope.myNote);
+					note = (isNaN(note)) ? 5 : note;
+					note = (note > 5) ? 5 : note;
+					note = (note < 0) ? 0 : note;
+					var html = '';
+					for (var i = 0; i < note; i++) {
+						html += '<img src="os-widget/img/yellow_star.png" />';
+					}
+					for (var i = note; i < 5; i++) {
+						html += '<img src="os-widget/img/white_star.png" />';
+					}
+					element.html(html);
+				});
+				
 			}
 		};
 	});
