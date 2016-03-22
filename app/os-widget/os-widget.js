cb2f9e618371e6fc47531dd41aac70db68d3cc15
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-03-22 14:38:17 +0100

    ok

--- app/os-widget/os-widget.js
+++ app/os-widget/os-widget.js
@@ -3,7 +3,10 @@
 
 	var app = angular.module('os-widget', []);
 
-	app.directive('osStar', function() {
+	app.directive('osStar', [ '$injector', function($injector) {
+	
+		var $compile = $injector.get('$compile');
+		
 		return {
 			restrict: 'CEA',
 			scope: {
@@ -12,6 +15,11 @@
 			link: function(scope, element, attrs) {
 				console.log('os-star link', arguments);
 				
+				scope.update = function(note) {
+					console.log('update');
+					scope.note = note;
+				};
+				
 				scope.$watch('note', function() {
 					var note = Number(scope.note);
 					note = (isNaN(note)) ? 5 : note;
@@ -19,17 +27,18 @@
 					note = (note < 0) ? 0 : note;
 					var html = '';
 					for (var i = 0; i < note; i++) {
-						html += '<img src="os-widget/img/yellow_star.png" />';
+						html += '<img ng-click="update(' + (i+1) + ')" src="os-widget/img/yellow_star.png" />';
 					}
 					for (var i = note; i < 5; i++) {
-						html += '<img src="os-widget/img/white_star.png" />';
+						html += '<img ng-click="update(' + (i+1) + ')" src="os-widget/img/white_star.png" />';
 					}
 					element.html(html);
+					$compile(element.contents())(scope);
 				});
 				
 			}
 		};
-	});
+	}]);
 	
 	
 })();
