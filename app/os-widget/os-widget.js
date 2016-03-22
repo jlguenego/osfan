f894701c026cde5aa5411f005467797dfc2b68f7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-03-22 14:10:48 +0100

    5 0 limit

--- app/os-widget/os-widget.js
+++ app/os-widget/os-widget.js
@@ -6,9 +6,11 @@
 	app.directive('osStar', function() {
 		return {
 			restrict: 'CEA',
-			link: function(scope, element) {
+			link: function(scope, element, attrs) {
 				console.log('os-star link', arguments);
-				var note = 3;
+				var note = Number(attrs.note);
+				note = (note > 5) ? 5 : note;
+				note = (note < 0) ? 0 : note;
 				var html = '';
 				for (var i = 0; i < note; i++) {
 					html += '<img src="os-widget/img/yellow_star.png" />';
