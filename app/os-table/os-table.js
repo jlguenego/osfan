0c8b344970a7e1662429b6d2c1907864e193c119
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-03-23 10:18:17 +0100

    header ok

--- app/os-table/os-table.js
+++ app/os-table/os-table.js
@@ -6,12 +6,33 @@
 	app.directive('osTable', [ '$injector', function($injector) {
 	
 		var $compile = $injector.get('$compile');
+		var $http = $injector.get('$http');
 		
 		return {
 			restrict: 'CEA',
 			link: function(scope, element, attrs) {
 				console.log('os-table link', arguments);
-				
+				var src = attrs.src;
+				$http.get(src).then(function(response) {
+					var data = Papa.parse(response.data).data;
+					console.log('data', data);
+					var table = angular.element('<table></table>');
+					var header = angular.element('<thead></thead>');
+					var row = angular.element('<tr></tr>');
+					for (var i = 0; i < data[0].length; i++) {
+						console.log('i', i);
+						var label = data[0][i];
+						var th = angular.element('<th></th>');
+						th.append(label);
+						row.append(th);
+					}
+					console.log('row', row);
+					header.append(row);
+					table.append(header);
+					element.append(table);
+				}).catch(function(error) {
+					console.error('error', error)
+				});
 				
 				
 			}
